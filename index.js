b102a62c80e0c97d491988af52493db968d35c7c
MichaelBrownui <iqp4jf274u9v@hotmail.com>
2025-01-12 08:18:05 +0000

    implement multiple rarity calculation methods

--- index.js
+++ index.js
@@ -44,16 +44,28 @@ class NFTRarityAnalyzer {
         console.log('Attribute analysis complete');
     }
 
-    calculateRarity(tokenId) {
+    calculateRarity(tokenId, method = 'statistical') {
         const nft = this.collection.find(item => item.token_id === tokenId);
         if (!nft) {
             console.log('NFT not found');
             return null;
         }
 
-        let totalRarity = 0;
         const collectionSize = this.collection.length;
+        
+        switch(method) {
+            case 'statistical':
+                return this.calculateStatisticalRarity(nft, collectionSize);
+            case 'trait_normalization':
+                return this.calculateNormalizedRarity(nft, collectionSize);
+            default:
+                return this.calculateStatisticalRarity(nft, collectionSize);
+        }
+    }
 
+    calculateStatisticalRarity(nft, collectionSize) {
+        let totalRarity = 0;
+        
         if (nft.attributes) {
             nft.attributes.forEach(attr => {
                 const traitType = attr.trait_type;
@@ -67,9 +79,27 @@ class NFTRarityAnalyzer {
         }
 
         const averageRarity = totalRarity / nft.attributes.length;
-        console.log(`Average rarity: ${averageRarity.toFixed(2)}%`);
-        
+        console.log(`Statistical Rarity Score: ${averageRarity.toFixed(2)}%`);
         return averageRarity;
+    }
+
+    calculateNormalizedRarity(nft, collectionSize) {
+        let rarityScore = 0;
+        
+        if (nft.attributes) {
+            nft.attributes.forEach(attr => {
+                const traitType = attr.trait_type;
+                const value = attr.value;
+                const traitCount = this.attributes[traitType][value];
+                const traitRarity = 1 / traitCount;
+                
+                rarityScore += traitRarity;
+                console.log(`${traitType}: ${value} (Rarity Weight: ${traitRarity.toFixed(4)})`);
+            });
+        }
+
+        console.log(`Normalized Rarity Score: ${rarityScore.toFixed(4)}`);
+        return rarityScore;
     }
 }
 
